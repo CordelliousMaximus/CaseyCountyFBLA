d6edb8cb2b91dc07e801ecc945ba8687029ed706
CordelliousMaximus <91574169+CordelliousMaximus@users.noreply.github.com>
2022-03-14 21:22:37 -0400

    changed buissness to business
    fixed grammar mistakes and made IFrame adaptable and responsive.

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,12 @@
 */
 // Highlights current date on contact page
 // Tells if store is open
-'use strict'
 window.addEventListener('DOMContentLoaded', event => {
-    const openingHours = [[0,0], [0, 0], [8, 18], [8, 18], [8, 18], [8, 18], [8, 13]];
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     const openClosed = document.getElementById("open-closed");
-    let date = new Date().getDay();
-    let hours = new Date().getHours(); 
+    let date = new Date().getDay(); 
 
-    if(date > 0){
-        listHoursArray[date - 1].classList.add(('today'));
-    }
-    else {
-        listHoursArray[6].classList.add('today');
-    }
+    listHoursArray[new Date().getDay()].classList.add(('today'));
 
-    if(date == 0 || date == 1){
-        openClosed.innerHTML = "Sorry we are currently closed";
-    }
-    else {
-        if(hours > openingHours[date][0] && hours < openingHours[date][1]){
-            return;
-        }
-        else{
-            openClosed.innerHTML = "Sorry we are currently closed";
-        }
-    }
+    (date == 0 || date == 1) ? (openClosed.innerHTML = "Sorry we are currently closed") : null;
 });
